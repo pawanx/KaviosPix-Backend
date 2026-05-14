43a29c315fb93cf969377174cc4ed811c3d76116
Pawan <pawanmishra196@gmail.com>
2026-05-14 13:12:48 +0530

    file upload fix

--- middleware/uploadMiddleware.js
+++ middleware/uploadMiddleware.js
@@ -1,11 +1,23 @@
 const multer = require("multer");
 const path = require("path");
+const fs = require("fs");
+
 
 // --------------- Storage config -------------
+const uploadPath = path.join(
+  __dirname,
+  "../uploads",
+);
+
+if (!fs.existsSync(uploadPath)) {
+  fs.mkdirSync(uploadPath, {
+    recursive: true,
+  });
+}
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     //if null error save to uploads folder
-    cb(null, "uploads/");
+    cb(null, uploadPath);
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + path.extname(file.originalname));
