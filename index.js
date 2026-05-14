119e240d9cff111a8a79551e944ada36d6a624b7
Pawan <pawanmishra196@gmail.com>
2026-05-14 13:51:30 +0530

    fixed cors

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://kavios-pix-frontend-one.vercel.app",
+    origin:["https://kavios-pix-frontend-one.vercel.app","http://localhost:5173"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true, // I am using auth
   }),
